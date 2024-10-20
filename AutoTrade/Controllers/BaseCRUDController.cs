3893a214a7bd449216d68422a41d37078dafc0f0
Haris Likić <haris.likic@hotmail.com>
2024-10-20 17:52:20 +0200

    update

--- AutoTrade/Controllers/BaseCRUDController.cs
+++ AutoTrade/Controllers/BaseCRUDController.cs
@@ -8,26 +8,23 @@ namespace Controllers
 {
     public class BaseCRUDController<TModel, TSearch, TInsert, TUpdate> : BaseController<TModel, TSearch> where TSearch : BaseSerachObject where TModel : class
     {
+
+        protected new ICRUDService<TModel, TSearch, TInsert, TUpdate> _service;
         public BaseCRUDController(ICRUDService<TModel, TSearch, TInsert, TUpdate> service) : base(service)
         {
-
+            _service = service;
         }
 
         [HttpPost]
         public virtual TModel Insert(TInsert request)
         {
-            if (_service == null)
-            {
-                throw new NullReferenceException("User service is not initialized.");
-            }
-
-            return (_service as ICRUDService<TModel, TSearch, TInsert, TUpdate>).Insert(request);
+            return _service.Insert(request);
         }
 
         [HttpPut("{id}")]
         public TModel Update(int id, TUpdate request)
         {
-            return (_service as ICRUDService<TModel, TSearch, TInsert, TUpdate>).Update(id, request);
+            return _service.Update(id, request);
         }
     }
 }
