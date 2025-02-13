c33ed9c7ba59c3621eb194d8eb7c4904121a8dbc
Haris Likić <haris.likic@hotmail.com>
2025-02-13 21:06:38 +0100

    fix dependency

--- AutoTrade/AutoTrade.Services/ExpiredHighlightsService.cs
+++ AutoTrade/AutoTrade.Services/ExpiredHighlightsService.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using AutoTrade.Services.Database;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 public class ExpiredHighlightsService : BackgroundService
 {
